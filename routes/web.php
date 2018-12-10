0087bb457a84671df4580246a75b8789f96f49ae
Tonny H. Soerojo <tonny@soerojo.com>
2018-12-10 18:26:20 +0700

    AdminLTE Installed

--- routes/web.php
+++ routes/web.php
@@ -221,3 +221,7 @@ Route::get('portfolio-standard-4-col', function () {return view('pages.portfolio
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
