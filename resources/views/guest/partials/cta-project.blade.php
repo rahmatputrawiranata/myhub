1aeba1403760991590f9d3d54dbbee8ad0851918
Biang Poker <poker@ths-mac.local>
2018-12-14 08:54:17 +0700

    cta added

---
+++ resources/views/guest/partials/cta-project.blade.php
@@ -0,0 +1,64 @@
+<!-- Clients Grid -->
+<section class="section-pt pb-5" id="project">
+  <div class="row" >
+    <div class="col-sm-12">
+      <div class="heading-block text-center">
+        <br>
+        <h4 class="text-uppercase text-theme">Project HUB</h4>
+        <div class="divider"></div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 mb-4">
+        <div class="clients line">
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo1.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo2.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo3.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo1.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo2.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo3.png')}}" alt=""></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 mb-4">
+        <div class="clients line">
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo1.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo2.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo3.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo1.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo2.png')}}" alt=""></div>
+          </div>
+          <div class="col-sm-4 col-xs-6 col-xx-12">
+            <div class="media"><img class="img-responsive" src="{{ asset('templates/kkr021/images/clients/logo3.png')}}" alt=""></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="heading-block text-center">
+      <a href="project" class="btn btn-default btn-sm btn-outline dark-border fill-theme">More Detail <i class="fa fa-eye" aria-hidden="true"></i></a>
+    </div>
+  </div>
+</section>
+<!-- End Clients Grid -->
