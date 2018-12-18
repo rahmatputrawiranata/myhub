9dc7db57effd642c60be4b82dc8f05aa98110a21
Tonny H. Soerojo <tonny@soerojo.com>
2018-12-18 12:13:16 +0700

    clean code

--- resources/views/user/partials/networks/detail.blade.php
+++ resources/views/user/partials/networks/detail.blade.php
@@ -6,28 +6,28 @@
             <div class="box-body">
                 <div class="user-block">
                     <button type="button" class="btn btn-flat btn-default btn-xs pull-right">Follow</button>
-                    <img class="img-circle img-bordered-sm" src="http://localhost/myhub/public/img/user7-128x128.jpg" alt="User Image">
+                    <img class="img-circle img-bordered-sm" src="{{ asset('img/user7-128x128.jpg')}}" alt="User Image">
                     <span class="username">
                         <a href="#">Sarah Ross</a>
                     </span>
                 </div>
                 <div class="user-block">
                     <button type="button" class="btn btn-flat btn-default btn-xs pull-right">Follow</button>
-                    <img class="img-circle img-bordered-sm" src="http://localhost/myhub/public/img/user1-128x128.jpg" alt="User Image">
+                    <img class="img-circle img-bordered-sm" src="{{ asset('img/user1-128x128.jpg')}}" alt="User Image">
                     <span class="username">
                         <a href="#">Alexander Pierce</a>
                     </span>
                 </div>
                 <div class="user-block">
                     <button type="button" class="btn btn-flat btn-default btn-xs pull-right">Follow</button>
-                    <img class="img-circle img-bordered-sm" src="http://localhost/myhub/public/img/user4-128x128.jpg" alt="User Image">
+                    <img class="img-circle img-bordered-sm" src="{{ asset('img/user4-128x128.jpg')}}" alt="User Image">
                     <span class="username">
                         <a href="#">Luna Stark</a>
                     </span>
                 </div>
                 <div class="user-block">
                     <button type="button" class="btn btn-flat btn-default btn-xs pull-right">Follow</button>
-                    <img class="img-circle img-bordered-sm" src="http://localhost/myhub/public/img/user3-128x128.jpg" alt="User Image">
+                    <img class="img-circle img-bordered-sm" src="{{ asset('img/user3-128x128.jpg')}}" alt="User Image">
                     <span class="username">
                         <a href="#">Maria Gonzales</a>
                     </span>
