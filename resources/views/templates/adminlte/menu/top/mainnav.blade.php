54d062a8b632c535230c45a89b86990dac77e120
Tonny H. Soerojo <tonny@soerojo.com>
2018-12-18 09:41:25 +0700

    Main Navigation updated

--- resources/views/templates/adminlte/menu/top/mainnav.blade.php
+++ resources/views/templates/adminlte/menu/top/mainnav.blade.php
@@ -4,37 +4,24 @@
     <i class="fa fa-home"></i> home  <span class="sr-only">(current)</span></a>
   </li>
   <li class="dropdown messages-menu">
-    <a href="#" class="dropdown-toggle btn btn-flat" data-toggle="dropdown">
-      <i class="fa fa-share-alt fa-lg"></i> 
+    <a href="{{ url('networks')}}" class="btn btn-flat">
+      <i class="fa fa-share-alt fa-lg"></i> REMOTE HUB
     </a>
-    <ul class="dropdown-menu">
-      <li class="header"><a href="{{ url('networks')}}">REMOTE HUB</a></li>
-    </ul>
   </li>
   <li class="dropdown messages-menu">
-    <a href="#" class="dropdown-toggle btn btn-flat" data-toggle="dropdown">
-      <i class="fa fa-briefcase fa-lg"></i> 
+    <a href="{{ url('jobs')}}" btn btn-flat">
+      <i class="fa fa-briefcase fa-lg"></i> JOBS HUB
     </a>
-    <ul class="dropdown-menu">
-      <li class="header"><a href="{{ url('jobs')}}">JOBS HUB</a></li>
-    </ul>
   </li>
   <li class="dropdown messages-menu">
-    <a href="#" class="dropdown-toggle btn btn-flat" data-toggle="dropdown">
-      <i class="fa fa-graduation-cap fa-lg"></i> 
+    <a href="{{ url('learning')}}" btn btn-flat">
+      <i class="fa fa-graduation-cap fa-lg"></i> LEARNING HUB
     </a>
-    <ul class="dropdown-menu">
-      <li class="header"><a href="{{ url('learning')}}">LEARNING HUB</a></li>
-    </ul>
   </li>
   <li class="dropdown messages-menu">
-    <a href="#" class="dropdown-toggle btn btn-flat" data-toggle="dropdown">
-      <i class="fa fa-exchange fa-lg"></i> 
+    <a href="{{ url('market')}}" btn btn-flat">
+      <i class="fa fa-exchange fa-lg"></i> MARKET HUB
     </a>
-    <ul class="dropdown-menu">
-      <li class="header"><a href="{{ url('market')}}">MARKET HUB</a></li>
-    </ul>
-  </li>
 </ul>
   {{-- @include('templates.adminlte.menu.top.remotehub') --}}
   {{-- @include('templates.adminlte.menu.top.jobshub') --}}
