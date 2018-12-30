ff5a08003784c1b0e40340f6d6a2ebdd01e985f5
Tonny H Soerojo <tonny@soerojo.com>
2018-12-30 10:19:04 +0700

    time line updated

--- resources/views/pages/user/wall.blade.php
+++ resources/views/pages/user/wall.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.tpl-adminsb')
 
 @section('title')
-Timeline
+Hub Info
 @endsection
 
 @section('extra-css')
@@ -31,180 +31,44 @@ Timeline
     <div class="row clearfix">
         <div class="col-xs-12">
             <div class="card">
+                <div class="header">
+                    <h2 class="text-uppercase">
+                        @yield('title')
+                    </h2>
+                    <ul class="header-dropdown m-r--5">
+                        <li class="dropdown">
+                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <i class="material-icons">more_vert</i>
+                            </a>
+                            <ul class="dropdown-menu pull-right">
+                                <li><a href="{{ url('/home')}}" class=" waves-effect waves-block">Home</a></li>
+                                <li><a href="{{ url('remotehub/home')}}" class=" waves-effect waves-block">REMOTEHUB</a></li>
+                                <li><a href="{{ url('events/home')}}" class=" waves-effect waves-block">EVENTS</a></li>
+                                <li><a href="{{ url('market/home')}}" class=" waves-effect waves-block">MARKETPLACE</a></li>
+                                <li><a href="{{ url('jobs/home')}}" class=" waves-effect waves-block">JOBS</a></li>
+                                <li><a href="{{ url('user/home')}}" class=" waves-effect waves-block">PROFILE</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
                 <div class="body">
                     <div>
-                        <div class="pull-right">
-                            @yield('title')
-                        </div>
                         <ul class="nav nav-tabs" role="tablist">
                             <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+                            <li role="presentation"><a href="#my_post" aria-controls="settings" role="tab" data-toggle="tab">My Post</a></li>
                             <li role="presentation"><a href="#profile_settings" aria-controls="settings" role="tab" data-toggle="tab">Profile Settings</a></li>
                             <li role="presentation"><a href="#change_password_settings" aria-controls="settings" role="tab" data-toggle="tab">Change Password</a></li>
                         </ul>
                         
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane fade in active" id="home">
-                                <div class="panel panel-default panel-post">
-                                    <div class="panel-heading">
-                                        <div class="media">
-                                            <div class="media-left">
-                                                <a href="#">
-                                                    <img src="{{ asset('img/user-lg.jpg')}}" />
-                                                </a>
-                                            </div>
-                                            <div class="media-body">
-                                                <h4 class="media-heading">
-                                                    <a href="#">Marc K. Hammond</a>
-                                                </h4>
-                                                Shared publicly - 26 Oct 2018
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="panel-body">
-                                        <div class="post">
-                                            <div class="post-heading">
-                                                <p>I am a very simple wall post. I am good at containing <a href="#">#small</a> bits of <a href="#">#information</a>. I require little more information to use effectively.</p>
-                                            </div>
-                                            <div class="post-content">
-                                                <img src="{{ asset('img/profile-post-image.jpg')}}" class="img-responsive" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="panel-footer">
-                                        <ul>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="material-icons">thumb_up</i>
-                                                    <span>12 Likes</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="material-icons">comment</i>
-                                                    <span>5 Comments</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="material-icons">share</i>
-                                                    <span>Share</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                        
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" placeholder="Type a comment" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="panel panel-default panel-post">
-                                    <div class="panel-heading">
-                                        <div class="media">
-                                            <div class="media-left">
-                                                <a href="#">
-                                                    <img src="{{ asset('img/user-lg.jpg')}}" />
-                                                </a>
-                                            </div>
-                                            <div class="media-body">
-                                                <h4 class="media-heading">
-                                                    <a href="#">Marc K. Hammond</a>
-                                                </h4>
-                                                Shared publicly - 01 Oct 2018
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="panel-body">
-                                        <div class="post">
-                                            <div class="post-heading">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                            </div>
-                                            <div class="post-content">
-                                                <iframe width="100%" height="360" src="https://www.youtube.com/embed/10r9ozshGVE" frameborder="0" allowfullscreen=""></iframe>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="panel-footer">
-                                        <ul>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="material-icons">thumb_up</i>
-                                                    <span>125 Likes</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="material-icons">comment</i>
-                                                    <span>8 Comments</span>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">
-                                                    <i class="material-icons">share</i>
-                                                    <span>Share</span>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                        
-                                        <div class="form-group">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" placeholder="Type a comment" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                @include('pages.user.wall.home')
+                            </div>
+                            <div role="tabpanel" class="tab-pane fade in" id="my_post">
+                                @include('pages.user.wall.my_post')
                             </div>
                             <div role="tabpanel" class="tab-pane fade in" id="profile_settings">
-                                <form class="form-horizontal">
-                                    <div class="form-group">
-                                        <label for="NameSurname" class="col-sm-2 control-label">Name Surname</label>
-                                        <div class="col-sm-10">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" id="NameSurname" name="NameSurname" placeholder="Name Surname" value="Marc K. Hammond" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="Email" class="col-sm-2 control-label">Email</label>
-                                        <div class="col-sm-10">
-                                            <div class="form-line">
-                                                <input type="email" class="form-control" id="Email" name="Email" placeholder="Email" value="example@example.com" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="InputExperience" class="col-sm-2 control-label">Experience</label>
-                                        
-                                        <div class="col-sm-10">
-                                            <div class="form-line">
-                                                <textarea class="form-control" id="InputExperience" name="InputExperience" rows="3" placeholder="Experience"></textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="InputSkills" class="col-sm-2 control-label">Skills</label>
-                                        
-                                        <div class="col-sm-10">
-                                            <div class="form-line">
-                                                <input type="text" class="form-control" id="InputSkills" name="InputSkills" placeholder="Skills">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="form-group">
-                                        <div class="col-sm-offset-2 col-sm-10">
-                                            <input type="checkbox" id="terms_condition_check" class="chk-col-red filled-in" />
-                                            <label for="terms_condition_check">I agree to the <a href="#">terms and conditions</a></label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="col-sm-offset-2 col-sm-10">
-                                            <button type="submit" class="btn btn-danger">SUBMIT</button>
-                                        </div>
-                                    </div>
-                                </form>
+                                @include('pages.user.wall.profile')
                             </div>
                             <div role="tabpanel" class="tab-pane fade in" id="change_password_settings">
                                 <form class="form-horizontal">
---
+++ resources/views/pages/user/wall/profile.blade.php
@@ -0,0 +1,3 @@
+<p>
+    @include('pages.user.detail.experience')
+</p>
